76a6f4df6ee4e431038949b0cfcef4e6c1e6072b
Carol Casagrande <ca_casag@yahoo.com.br>
2021-05-24 22:14:13 -0300

    aula3 first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
 
-function App() {
+import { api } from './service/api'
+
+interface IData {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const App: React.FC = () => {
+  const [ data, setData ] = useState<IData[]>([]);
+  const [ name, setName ] = useState<string>('')
+
+  useEffect( () => {
+    api.get('data').then(
+      response => {
+        setData(response.data)
+      }
+    )
+  }, [] );
+
+  const convertoToCurrency = useCallback(
+    (value: number) => {
+      Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL'})
+      .format(value)
+    }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <ul>
+        { data.map(fruta => (
+          <li key={fruta.id}>
+            {fruta.name} | {convertoToCurrency(fruta.price)}
+          </li>
+      ))}
+
+      
+      </ul>
+      <hr />
+      <h1>{name}</h1>
+
+      <hr />
+
+      <input type="text" onChange={ e => setName(e.target.value)} placeholder="informe seu nome"/>
     </div>
   );
 }
